1363231bbb5c484a379ab6caa3ab27c5047d223d
KOUDJIL Khayri Anis <koudjilkhayrianis@gmail.com>
2023-06-10 08:31:18 +0100

    add another option for headless

--- parse_js.py
+++ parse_js.py
@@ -10,8 +10,12 @@ def parse_js(
 ):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
     driver = webdriver.Chrome(options=chrome_options)
-    # driver = webdriver.Chrome()
 
     driver.get(url)
     time.sleep(30)
